6bfb5ed0b495076187659711331a06f20fae769c
Fábio <fabio.so2307@hotmail.com>
2026-04-01 00:07:26 -0300

    ajustando o getAds 20

--- api/test.js
+++ api/test.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
 
         const supabase = createClient(
             process.env.SUPABASE_URL,
-            process.env.SUPABASE_KEY
+            process.env.SUPABASE_SERVICE_ROLE_KEY
         );
 
         const { data, error } = await supabase
