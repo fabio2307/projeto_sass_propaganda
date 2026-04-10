c18e1a2ef5023dba415efde602ff3610b641677c
Fábio <fabio.so2307@hotmail.com>
2026-04-10 01:14:16 -0300

    ajustando a forma de pagamento 101

--- api/index.js
+++ api/index.js
@@ -328,7 +328,7 @@ export default async function handler(req, res) {
 
             try {
                 const session = await stripe.checkout.sessions.create({
-                    payment_method_types: ["card"],
+                    payment_method_types: ["card", "pix"], // ✅ agora aceita PIX
                     mode: "payment",
                     line_items: [{
                         price_data: {
